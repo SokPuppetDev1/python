f6787e680f34c3abd4cdb2bc51dccecc994198c9
SokPuppetDev1 <crolfe@salisbury6c.ac.uk>
2016-12-08 14:36:20 +0000

    edit update

--- square.py
+++ square.py
@@ -1,6 +1,11 @@
+'''imports needed'''
 import library as l
 
+
+'''creating global vairables'''
 mem = []
+save_type = ".p"
+
 
 '''visually printing the rectangle'''
 def printr():
@@ -8,56 +13,62 @@ def printr():
     count = l.getnum(True)
     for i in range(count):
         print("which rectangle would you like to print")
-        t = l.getnum(False,0,len(mem)-1)
+        t = l.getnum(False, 0, len(mem) - 1)
         mem[t].show()
+
 
 '''removes a rectangle from the list'''
 def delete():
-    conf=["YES","NO"]
-    print("how many rectagles would you like to delete")
-    count = l.getnum(True)
-    i=0
-    while i < count:
-        valid = False
-        print("which rectangle would you like to delete")
-        t = l.getnum(False,0,len(mem)-1)
-        data(mem[t],mem)
-        while valid == False:
-            print("""are you sure you would like to delete this rectangle
+    conf = ["YES", "NO"] 
+    menu = """are you sure you would like to delete this rectangle
 (type exactly what is in the brackets)
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ({0[0]})--yes
 ({0[1]})---no
-""".format(conf))
-            s=input()
-            for x in conf:
-                if s == x:
-                    valid = True
-        
-            if valid == True:
-                if s == conf[0]:
-                    mem.remove(mem[t])
-                    i=i+1
-                elif s == conf[1]:
-                    print("please chose a different rectangle then")
-            else:
-                print("please give a valid input")
+""".format(conf)
+    print("how many rectagles would you like to delete")
+    count = l.getnum(True)
+    i = 0
+    while i < count:
+        print("which rectangle would you like to delete")
+        t = l.getnum(False, 0, len(mem) - 1)
+        data(mem[t], mem)
+        usr = validate(conf, menu)
+        if usr == conf[0]:
+            mem.remove(mem[t])
+            i += 1
+        elif usr == conf[1]: print("please chose a different rectangle then")
+
 
 '''calls the function for creating rectangle(s)'''
 def create():
-    measurements=['length','hight']
+    measurements = ['length','hight']
     print('how many times do you want to create a rectangle')
-    amount=l.getnum(True)
+    amount = l.getnum(True)
+    x = len(mem)
     for i in range(amount):
-        re=[]
+        re = []
         for mes in measurements:
-            print('what is the {0} of rectangle {1}'.format(mes,i))
+            print('what is the {0} of rectangle {1}'.format(mes, x + i))
             re.append(l.getnum(True))
-        rectangle=l.Rectangle(re[0],re[1])
+        rectangle = l.Rectangle(re[0], re[1])
         mem.append(rectangle)
 
+
+'''checking if an input is the same as an item in a list of objects'''
+def validate(listx, text):
+    validating = True
+    print(text)
+    while validating == True:
+        usr = input("-")
+        for x in listx:
+            if usr == x: validating = False
+        if validating == True: print("\nplease give a valid input\n")
+    return usr                
+
+
 '''prints out the data for rectangles'''
-def data(ob,li):
+def data(ob, li):
     print("----------------------------------")
     print("rectangle-{0}".format(li.index(ob)))
     print("length----{0}".format(ob.length))
@@ -66,60 +77,64 @@ def data(ob,li):
     print("parimiter-{0}".format(ob.parimiter))
     print("----------------------------------")
    
-def mod():
-    print("WIP")
 
+'''the function for modifying rectangles'''
+def mod(): print("WIP")
+
+
+'''checks for a certain save type at the end of the file'''
+def file_type(t):
+    is_type = False
+    while is_type == False:
+        usr = input("-")
+        if usr.find(t, len(usr) - len(t)) != -1: is_type = True
+        else: print("please put {0} at the end of the file name".format(t))
+    return usr
+
+
+'''loads a data from a file'''
 def load():
     accessing = True
     while accessing == True:
-        file_name = input("file (.p at the end of the file)\n")
+        print("""please give a file name
+({0} at the end of the file)
+""".format(save_type))
+        file_name = file_type(save_type)
         try:
-            with open(file_name,'rb') as use:
-                test = l.pickle.load(use)
+            with open(file_name, 'rb') as use:
+                loading = l.pickle.load(use)
                 print("loading these objects")
-                for i in test:
-                    data(i,test)
+                for i in loading:
+                    data(i, loading)
                     mem.append(i)
             accessing = False
-        except FileNotFoundError:
-            print("file doenst exist")
+        except FileNotFoundError: print("file doenst exist")
 
+
+'''saves the data to a file'''
 def save():
-    accessing = True
-    while accessing == True:
-        file_name = input("file (.p at end of file name)\n")
-        try:
-            with open(file_name,'rb') as use:
-                test = l.pickle.load(use)
-                for i in test:
-                    exist = False
-                    for j in mem:
-                        if i == j:
-                            exist = True
-                    
-                    if exist != True:
-                        mem.append(i)
+    print("""please give a file name
+({0} at the end of the file)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+##############################   
+#THIS WILL OVERWRITE THE FILE#
+##############################
+""".format(save_type))
+    file_name = file_type(save_type)
+    with open(file_name, 'wb') as use: l.pickle.dump(mem, use)
+    print("save compleate")
 
-            with open(file_name,'wb') as use:
-                l.pickle.dump(mem,use)
-            accessing = False
-        except FileNotFoundError:
-            print("creating new file")
-            with open(file_name, 'wb') as use:
-                l.pickle.dump(mem,use)
-            accessing = False
-        print("save compleate")
 
 '''check rectangles'''
 def check():
     print('\nthere are {0} rectangles\n'.format(len(mem)))
     running = True
-    paramt = ["length","height","area","parimiter"]
-    options = ['LENGTH','HEIGHT','AREA','PARIMITER','ALL','INDEX','BACK']
-    while running == True:
-        print("""what would you like to check
+    paramt = ["length", "height", "area", "parimiter"]
+    options = ['LENGTH', 'HEIGHT', 'AREA',
+               'PARIMITER', 'ALL', 'INDEX', 'BACK']
+    menu = """what would you like to check
 (type exactly what is in the brackets)
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ({0[0]})----details of all rectangles with a spesific {1[0]}
 ({0[1]})----details of all rectangles with a spesific {1[1]}
 ({0[2]})------details of all rectangles with a spesific {1[2]}
@@ -127,72 +142,54 @@ def check():
 ({0[4]})-------gives back the details of all the rectangles
 ({0[5]})-----give the details of a single rectangle
 ({0[6]})------go back to main menu
-""".format(options,paramt))
-        choice = input()
-        valid = False
-        for h in options:
-            if h == choice:
-                valid = True
+""".format(options, paramt)
+    validate(options, menu)
+    for y in range(len(paramt)):
+        if choice == options[y]:
+            print("what is the {0} you would like to search for".format(
+                  paramt[y]))
+            inp = l.getnum(True)
+            for x in mem:
+                param = [x.length, x.height, x.area, x.parimiter]
+                if param[y] == inp: data(x, mem)
+    if choice == options[4]:
+        for x in mem: data(x, mem)
+    elif choice == options[5]:
+        print("what rectangle would you like to search for")
+        inp = l.getnum(False, 0, len(mem)-1)
+        data(mem[inp], mem)
+    elif choice == options[6]: running = exit()
 
-        if valid == True:
-            for y in range(len(paramt)):
-                if choice == options[y]:
-                    print("what is the {0} you would like to search for".format(paramt[y]))
-                    inp = l.getnum(True)
-                    for x in mem:
-                        param = [x.length,x.height,x.area,x.parimiter]
-                        if param[y] == inp:
-                            data(x,mem)
-            if choice == options[4]:
-                for x in mem:
-                    data(x,mem)
-            elif choice == options[5]:
-                print("what rectangle would you like to search for")
-                inp = l.getnum(False,0,len(mem)-1)
-                data(mem[inp],mem)
-            elif choice == options[6]:
-                running = exit()
-        else:
-            print("\nplease give a valid input\n")
 
+'''the exit function'''
 def exit(): return False
+
 
 '''the interface'''
 def menu():
-
-    funct = [create,check,delete,mod,printr,load,save]
+    funct = [create, check, delete,
+             mod, printr, load, save]
     running = True
-    options = ['CREATE','CHECK','DELETE','MOD','PRINT','LOAD','SAVE','EXIT']
-
-    while running == True:
-        print("""what would you like to do?
+    options = ['CREATE', 'CHECK', 'DELETE', 'MOD',
+               'PRINT', 'LOAD', 'SAVE', 'EXIT']
+    menu = """what would you like to do?
 (type exactly what is in the brackets)
-
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ({0[0]})----create rectangles
 ({0[1]})-----check what rectangles exist
 ({0[2]})----delete existing rectangles
-({0[3]})-------modify a rectangle
+({0[3]})-------modify the lengths and heights of a rectangle
 ({0[4]})-----print out a pictute of the rectangle on the screen
 ({0[5]})------load a file of existing rectangles
 ({0[6]})------save the loaded rectangles to a file
 ({0[7]})------exit the program
-""".format(options))
-        choice = input()
-        valid = False
-        for h in options:
-            if h == choice:
-                valid = True
+""".format(options)
+    while running == True:
+        choice = validate(options, menu)
+        for f in funct:
+            if choice == options[funct.index(f)]: f()
+        if choice == options[7]: running = exit()
 
-        if valid == True:
-            fc=0
-            for f in funct:
-                if choice == options[fc]:
-                    f()
-                fc=fc+1
-            if choice == options[7]:
-                running = exit()
-        else:
-            print('\nplease give a valid input\n')
 
 '''main program'''
 if __name__ == '__main__':
